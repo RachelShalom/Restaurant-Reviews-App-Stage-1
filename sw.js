a8d4a73f9cd962b38923fbb9d32a6434ffd155d1
Rachel <rshalom1984@gmail.com>
2018-07-07 22:14:23 +0300

    sw small change

--- sw.js
+++ sw.js
@@ -1,28 +1,18 @@
-const staticCacheName = 'review-cache-v3';
+const staticCacheName = 'review-cache-v1';
 //this is all the urls to save in the cache
 var urlsToCache = ['/', '/css/styles.css',
     '/img/1.jpg', '/img/2.jpg',
     '/img/3.jpg', '/img/4.jpg',
     '/img/5.jpg', '/img/6.jpg',
     '/img/7.jpg', '/img/8.jpg',
-    '/img/9.jpg', '/img/10.jpg',
+    '/img/9.jpg', '/img/10.jpg', '/js/',
     '/js/dbhelper.js', '/js/restaurant_info.js',
     '/index.html', '/restaurant.html',
     '/data/restaurants.json', '/js/main.js',
     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js', 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
     'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png', 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon-2x.png',
-    'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png', 'https://leafletjs.com/reference-1.3.0.html#marker',
-    '/restaurant.html?id=1',
-    '/restaurant.html?id=2',
-    '/restaurant.html?id=3',
-    '/restaurant.html?id=4',
-    '/restaurant.html?id=5',
-    '/restaurant.html?id=6',
-    '/restaurant.html?id=7',
-    '/restaurant.html?id=8',
-    '/restaurant.html?id=9',
-    '/restaurant.html?id=10'
-
+    'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
+    'https://leafletjs.com/reference-1.3.0.html#marker'
 
 ]; ///////
 //open a cache whiile installing
